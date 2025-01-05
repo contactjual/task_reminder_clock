5a2f97d97ff43890c5acbda15d20f1f09d2f5fec
Jual Rana <contactjual@gmail.com>
2025-01-06 00:05:46 +0600

    task added not dynamicaly]

--- index.php
+++ index.php
@@ -16,8 +16,12 @@
       <!-- task for daylight -->
     <div id="daylight">
       <h2>Tasks on day</h2>
+      <p>[<span id="from1">09</span>-<span id="to1">01</span>]-><span id="task1">Learn PHP</span></p>
+      <p>[<span id="from2">09</span>-<span id="to2">01</span>]-><span id="task2">Break for lunch</span></p>
+      <p>[<span id="from3">09</span>-<span id="to3">01</span>]-><span id="task3">Learn PHP</span></p>
+      <p>[<span id="from4">09</span>-<span id="to4">01</span>]-><span id="task4">Break for physical exercise</span></p>
     </div>
-  
+
     <div id="time_container" class="clock-container">
       <div class="day" id="day_name"><?php echo date("l")?></div>
       <div class="date" id="dayMonYear"><?php echo date("jS F Y")?></div>
@@ -36,9 +40,13 @@
         </div>
       </div>
     </div>
-  
+
     <div id="night">
       <h2>Tasks on night</h2>
+      <p>[<span id="from5">06</span>-<span id="to5">08</span>]-><span id="task5">Social media marketing</span></p>
+      <p>[<span id="from6">09</span>-<span id="to6">01</span>]-><span id="task6">Break for dinner</span></p>
+      <p>[<span id="from7">09</span>-<span id="to7">01</span>]-><span id="task7">Learn PHP and [PS:30 - Pnt-Ms:30]</span></p>
+      <p>[<span id="from8">09</span>-<span id="to8">01</span>]-><span id="task8">Break for rest</span></p>
     </div>
   </main>
 
@@ -46,22 +54,36 @@
   <script>
 
   function time () {
-    // const timeContainer = document.getElementById("time_container");
 
+    // get timing 
     const date = new Date ();
     let getHour = date.getHours();
     let getmin = date.getMinutes();
     let getsec = date.getSeconds();
 
+    // set schedule funciton
+    setSchedule (getHour, getmin);
 
-
-    // document.getElementById("dayMonYear").innerText = ;
+    // set timing update
     document.getElementById("hour").innerText = getHour;
     document.getElementById("min").innerText = getmin;
     document.getElementById("second").innerText = getsec;
   }
   
   setInterval(time, 1000);
+
+  function setSchedule (hour, min) {
+    if (hour >= "09" && hour <= "01") {
+      document.getElementById("from1").innerText = "09";
+      document.getElementById("to1").innerText = "01";
+      document.getElementById("task1").innerText = "Learn PHP";
+    } 
+    if (hour >= "01" && hour <= "03") {
+      document.getElementById("from2").innerText = "01";
+      document.getElementById("to2").innerText = "03";
+      document.getElementById("task2").innerText = "Break for lunch";
+    } 
+  }
 
   </script>
 
